0ab474d5926ae9f662d264fcf027e7841fe845dd
Franek Mirkowski <mirkowskifranciszek@gmail.com>
2024-11-25 16:06:50 +0100

    added web analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PaperDiscoveryPlatform from './components/PaperDiscoveryPlatform';
 import './styles/App.css';
+import { Analytics } from '@vercel/analytics/react';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <PaperDiscoveryPlatform />
+      <Analytics />
     </div>
   );
 };
